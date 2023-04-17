3b84fc871c85c67fecd5556780cd5fd34999faa2
MichalKlyz-s <klyz.michal97@gmail.com>
2023-04-17 15:26:53 +0200

    Dodanie grania przez komputer przez midi wybranej nuty

--- Backend/src/controllers/midi.ts
+++ Backend/src/controllers/midi.ts
@@ -1,6 +1,37 @@
+import {WebMidi} from "webmidi";
+
 export const midi = (params: any, req: any) =>{
     console.log(params);
     console.log(req);
+    WebMidi
+    .enable()
+    .then(() => console.log("WebMidi enabled!"))
+    .catch(err => alert(err));
+  
+    // WebMidi
+    // .enable()
+    // .then(onEnabled)
+    // .catch(err => alert(err));
+  
+//   function onEnabled() {
+    
+//     // Inputs
+//     WebMidi.inputs.forEach(input => console.log("in" + input.manufacturer, input.name));
+    
+//     // Outputs
+//     WebMidi.outputs.forEach(output => console.log(output.name));
+  
+//   }
+  WebMidi
+  .enable()
+  .then(test)
+  .catch(err => alert(err));
+  function test(){
+    let myOutput = WebMidi.getOutputByName("Microsoft GS Wavetable Synth");
+    console.log("Moje"+ myOutput.name);
+    let channel = myOutput.channels[1];
+    channel.playNote(params, {duration: 1000});
+  }
     const answer = {
         'succes': true,
         'user': 'mk'
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,18 +3,54 @@ import cors from 'cors';
 import * as fs from 'fs';
 import {hay} from './hello';
 import * as midi from './controllers/midi'
+import {WebMidi} from "webmidi";
+// import test from "webmidi";
 const app = express();
-
 app.use(cors());
 
-app.get('/midi', async (req, response) => {
-    // console.log(req);
-    // response.send({'success': true})
+app.get('/midi', async (params, response) => {
+    console.log('wejscie');
+    // console.log(params.params);
+    console.log(params.query.note);
+    response.send({'success': true})
     // console.log(midi.midi('test', 'test'))
-    await response.send(midi.midi('test', 'test'));
-    // response.status(200).end();
+    midi.midi(params.query.note, "test")
+    response.status(200).end();
+});
+//     WebMidi
+//   .enable()
+//   .then(() => console.log("WebMidi enabled!"))
+//   .catch(err => alert(err));
+
+//   WebMidi
+//   .enable()
+//   .then(onEnabled)
+//   .catch(err => alert(err));
+
+// function onEnabled() {
+  
+//   // Inputs
+//   WebMidi.inputs.forEach(input => console.log("in" + input.manufacturer, input.name));
+  
+//   // Outputs
+//   WebMidi.outputs.forEach(output => console.log(output.name));
+
+// }
+// WebMidi
+// .enable()
+// .then(test)
+// .catch(err => alert(err));
+// function test(){
+//   let myOutput = WebMidi.getOutputByName("Microsoft GS Wavetable Synth");
+//   console.log("Moje"+ myOutput.name);
+//   let channel = myOutput.channels[1];
+//   channel.playNote("C3", {duration: 1000});
+// }
+// myOutput.sendAllSoundOff();
+
     
-})
+// })
+
 app.listen(8888, () => {
     console.log('Aplikacja wystartowała')
 });
